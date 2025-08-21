b747a5359dede878202bfc42a53d6450afddb3a3
p2pdevmedia <80440258+p2pdevmedia@users.noreply.github.com>
2025-08-21 11:08:36 -0300

    feat: add basic internationalization

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,7 +2,12 @@
 
 import { SessionProvider } from 'next-auth/react';
 import type { ReactNode } from 'react';
+import { LanguageProvider } from './language-provider';
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <LanguageProvider>{children}</LanguageProvider>
+    </SessionProvider>
+  );
 }
