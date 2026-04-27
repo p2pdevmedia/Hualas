d486213f2844fa00acd09636ea255213a0a81232
Ivan Muller <mullerivan@gmail.com>
2026-04-27 19:18:40 -0300

    feat: add indexes to AccountingMovement (date, createdById)

---
+++ prisma/migrations/20260427040001_add_accounting_movement_indexes/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "AccountingMovement_date_idx" ON "AccountingMovement"("date");
+
+-- CreateIndex
+CREATE INDEX "AccountingMovement_createdById_idx" ON "AccountingMovement"("createdById");
